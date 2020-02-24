ff2a5cf5a580476b4a644714d37e42f8b4bd981d
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2020-02-24 21:26:26 +0800

    fixed stupid qyuqxun bug

--- Game/Game/government.cpp
+++ Game/Game/government.cpp
@@ -1,1 +1,24 @@
 #include "government.h"
+
+void government::co_bu()
+{
+    
+    area::action buildHosp = area::action::buildHospital;
+    money -= Area.addAction(buildHosp, 7);
+    day_in += 50000;
+    
+}
+
+void government::go_pe_give(int& gi_rate)//捐款，政府额外补贴部分,girate表示捐款率，每捐一次都会降低
+{
+    int ex_ge;//extra get
+    bool from;
+    //从哪里来的，政府=1，民众=0
+    if (from = false)
+    {
+        static std::default_random_engine e(seed);
+        static std::uniform_real_distribution<double> u(0, 1);
+        if (u(e) > 0.9)
+            ex_ge /= 1.5, day_in -= 20000, gi_rate +=/*!!!!没想好*/;//无良红十字会
+    }
+}
--- Game/Game/government.h
+++ Game/Game/government.h
@@ -5,29 +5,15 @@
 public:
     void ch_day()
 {
-    money+=day_in();
+    money+=day_in;
     money-=Area.getTodaySpend();
 }
-    void co_bu()
-    {
-    area::action buildHosp=buildHospital;
-    money -= Area.addAction(buildHosp, 7);
-    day_in+=50000;
-    }
-    void go_pe_give(&int gi_rate)//捐款，政府额外补贴部分,girate表示捐款率，每捐一次都会降低
-    {
-        int ex_ge;//extra get
-        bool from;
-        //从哪里来的，政府=1，民众=0
-        if(from=false)
-        {
-        static std::default_random_engine e(seed);
-        static std::uniform_real_distribution<double> u(0, 1);
-        if(u(e)>0.9)
-            ex_ge/=1.5,day_in-=20000,gi_rate+=/*!!!!没想好*/;//无良红十字会
-        }
-    }
+    void co_bu();
+
+    void go_pe_give(int& gi_rate);//捐款，政府额外补贴部分,girate表示捐款率，每捐一次都会降低
+
 private:
      area Area;
-
+     unsigned long long money;
+     unsigned long long day_in;
  };
