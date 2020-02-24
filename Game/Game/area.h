b4d44e40911e43cdec3b019a4ba8b5bf2a939d7a
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2020-02-24 15:27:32 +0800

    .

--- Game/Game/area.h
+++ Game/Game/area.h
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <utility>
 #include <vector>
+#include <iostream>
 class area
 {
 private:
@@ -29,7 +30,10 @@ private:
 
 public:
     enum action {buildHospital, reduceMovement};
-
-    std::vector<std::pair<action, std::int16_t>> actions;//消息队列，存放pair，pair内部是操作名称和还需时间。
+private:
+    std::vector<std::pair<action, int16_t>> actions;//消息队列，存放pair，pair内部是操作名称和还需时间。
+public:
+    inline void addAction(action _action, int16_t time) { actions.push_back(std::pair<action, int16_t>(_action, time)); }
+    inline void showAction() { for (auto& action_ : actions)std::cout << "Action:" << int(action_.first) << " Time:" << action_.second << " days.\n"; }
 };
 
